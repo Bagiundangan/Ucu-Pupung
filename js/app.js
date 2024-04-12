af1c7ac982d813cc6e22843b2bded76abc644ce7
Bagiundangan <135224162+Bagiundangan@users.noreply.github.com>
2024-04-13 06:21:32 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -266,9 +266,9 @@ const progress = (() => {
         loaded += 1;
 
         bar.style.width = Math.min((loaded / total) * 100, 100).toString() + "%";
-        info.innerText = `Loading gambar..(${loaded}/${total}) [${parseInt(bar.style.width).toFixed(0)}%]`;
+        info.innerText = `(${loaded}/${total}) [${parseInt(bar.style.width).toFixed(0)}%]`;
 
-        if (loaded == total) {
+        if (loaded >> total) {
             util.show();
         }
     };
